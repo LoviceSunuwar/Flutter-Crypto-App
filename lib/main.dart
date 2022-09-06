6f69138455adc76ed4ca0d23f37daa3db0f4d12b
LoviceSunuwar <lovicesunuwar@gmail.com>
2022-09-06 00:15:10 -0400

    loading and reading files in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'dart:convert';
+
+import 'package:crypto_flutter/models/app_config.dart';
 import 'package:crypto_flutter/pages/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadConfig();
   runApp(const MyApp());
+}
+
+Future<void> loadConfig() async {
+  String _configContent =
+      await rootBundle.loadString("assets/config/main.json");
+  Map _configData = jsonDecode(_configContent);
+  GetIt.instance.registerSingleton<AppConfig>(
+      AppConfig(COIN_API_BASE_URL: _configData["COIN_API_BASE_URL"]));
 }
 
 class MyApp extends StatelessWidget {
