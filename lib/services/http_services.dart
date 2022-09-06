66267e72bf955409ff908b97af713aaed8a94c06
LoviceSunuwar <lovicesunuwar@gmail.com>
2022-09-06 00:50:17 -0400

    get future fucntion on dio

--- lib/services/http_services.dart
+++ lib/services/http_services.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import '../models/app_config.dart';
 import 'package:get_it/get_it.dart';
 
@@ -12,5 +13,18 @@ class HTTPService {
     _appConfig = GetIt.instance.get<AppConfig>();
     _base_url = _appConfig!.COIN_API_BASE_URL;
     print(_base_url);
+  }
+
+  Future<Response?> get(String _path) async {
+    try {
+      String _url = "$_base_url$_path";
+      // print(_url);
+      Response _response = await dio.get(_url);
+      print(_response);
+      return _response;
+    } catch (e) {
+      print("Unable to perform a get request");
+      print(e);
+    }
   }
 }
