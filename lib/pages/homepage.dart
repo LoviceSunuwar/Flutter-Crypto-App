f24c5a4eb2f69b22e8d84368347255d9353a44e3
LoviceSunuwar <lovicesunuwar@gmail.com>
2022-09-08 02:24:56 -0400

    widget for the description container

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -49,8 +49,18 @@ class _HomepageState extends State<Homepage> {
         if (_snapshot.hasData) {
           Map _data = jsonDecode(_snapshot.data.toString());
           num _cadPrice = _data["market_data"]["current_price"]["cad"];
-          return Text(
-            _cadPrice.toString(),
+          num _change24h = _data["market_data"]["price_change_percentage_24h"];
+          String _coinDesc = _data["description"]["en"];
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _coinImageWidget(_data["image"]["large"]),
+              _currentPriceWidget(_cadPrice),
+              _percentageChangeWidget(_change24h),
+              _coinDescription(_coinDesc)
+            ],
           );
         } else {
           return const Center(
@@ -60,6 +70,48 @@ class _HomepageState extends State<Homepage> {
           );
         }
       },
+    );
+  }
+
+  Widget _currentPriceWidget(num _rate) {
+    return Text(
+      "${_rate.toStringAsFixed(2)}",
+      style: const TextStyle(
+          color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
+    );
+  }
+
+  Widget _percentageChangeWidget(num _change) {
+    return Text(
+      "${_change.toStringAsFixed(2)} %",
+      style: const TextStyle(
+          color: Colors.white, fontSize: 15, fontWeight: FontWeight.w400),
+    );
+  }
+
+  Widget _coinImageWidget(String _imgURL) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: _deviceHeight! * 0.2),
+      height: _deviceHeight! * 0.15,
+      width: _deviceWidth! * 0.15,
+      decoration:
+          BoxDecoration(image: DecorationImage(image: NetworkImage(_imgURL))),
+    );
+  }
+
+  Widget _coinDescription(String _coinDescription) {
+    return Container(
+      color: Color.fromARGB(255, 58, 93, 110),
+      height: _deviceHeight! * 0.45,
+      width: _deviceWidth! * 0.90,
+      margin: EdgeInsets.symmetric(vertical: _deviceHeight! * 0.05),
+      padding: EdgeInsets.symmetric(
+          vertical: _deviceHeight! * 0.01, horizontal: _deviceWidth! * 0.01),
+      child: Text(
+        _coinDescription,
+        style:
+            const TextStyle(color: Colors.white, fontWeight: FontWeight.w300),
+      ),
     );
   }
 
